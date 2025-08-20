3b01d910b7d5a95c37377b60ea3d31f10b2b4f33
Levente Hunyadi <hunyadi@gmail.com>
2025-08-20 11:51:57 +0200

    Publish to PyPI

--- markdown_doc/__init__.py
+++ markdown_doc/__init__.py
@@ -6,7 +6,7 @@ Copyright 2024-2025, Levente Hunyadi
 :see: https://github.com/hunyadi/markdown_doc
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "Levente Hunyadi"
 __copyright__ = "Copyright 2024-2025, Levente Hunyadi"
 __license__ = "MIT"
